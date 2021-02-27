5fa58258e31f57c4f6dbfbc8fc65e62cab2925f9
unknown <franciscogmarra@gmail.com>
2021-02-27 09:56:41 -0300

    se agrego js extra homeworks

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,13 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var keys = Object.keys(objeto);
+  var values = Object.values(objeto);
+
+  var matriz = keys.map(function(e, i){
+    return [e, values[i]];
+  });
+  return matriz;
 }
 
 
@@ -17,7 +24,23 @@ function numberOfCharacters(string) {
   //La función recibe un string. Recorre el srting y devuelve el caracter con el número de veces que aparece 
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
-  //Escribe tu código aquí
+  //Escribe tu código aquí:
+
+  var objeto = {};
+
+  for (let i = 0; i < string.length; i++){
+    let letra = string[i]
+    let value = Object.keys(objeto)
+    let isPresent = value.indexOf(letra) != -1
+    
+    if(!isPresent) {
+      objeto[letra] = 1;
+    }
+    else {
+      objeto[letra] += 1;
+  }
+}
+return objeto;
 }
 
 
@@ -25,7 +48,23 @@ function capToFront(s) {
   //Realiza una función que reciba como parámetro un string y mueva todas las letras mayúsculas
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
-  //Escribe tu código aquí
+  //Escribe tu código aquí:
+  var stringMayus = "";
+  var stringMinus = "";
+
+  for ( let i = 0; i < s.length; i++){
+    let upper = s[i].toUpperCase()
+        if (s[i] == upper){
+      stringMayus = stringMayus.concat(s[i]);
+    }
+  }
+  
+  for ( let y = 0; y < s.length; y++){
+    if (s[y] != s[y].toUpperCase()){
+      stringMinus = stringMinus.concat(s[y]);
+    }
+  }
+return stringMayus + stringMinus;
 }
 
 
@@ -34,15 +73,26 @@ function asAmirror(str) {
   //Escribe una función que tome la frase recibida y la devuelva de modo tal que se pueda leer de izquierda a derecha 
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
-  //Escribe tu código aquí
-} 
+  //Escribe tu código aquí:
+    const arr = str.split(" ");
+    const reversed = arr.map(function(el){
+      return el.split('').reverse().join("");
+    });
+    return reversed.join(" ");
+  }
 
 
 function capicua(numero){
   //Escribe una función, la cual recibe un número y determina si es o no capicúa.
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
-  //Escribe tu código aquí
+  //Escribe tu código aquí:
+  var arr = numero.toString().split("");
+  const reversed = arr.reverse();
+  if (numero == reversed.join("")){
+    return "Es capicua";
+  }
+  else return "No es capicua";
 }
 
 
@@ -50,13 +100,27 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var arr = cadena.split("")
+  for (i = 0; i < cadena.length; i++){
+    if (arr[i] == "a" || arr[i] == "b" || arr[i] == "c"){
+      arr.splice(i,1);
+      i--;
+    }
+  }
+ return arr.join("");
 }
 
 
 function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
-  //Escribe tu código aquí
+  //Escribe tu código aquí:
+arr.sort(function(a, b){
+  // ASC  -> a.length - b.length
+  // DESC -> b.length - a.length
+  return a.length - b.length;
+});
+return arr;
 }
 
 
@@ -65,7 +129,10 @@ function buscoInterseccion(arreglo1, arreglo2){
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí:
+  var arregloFinal = arreglo1.filter(v => arreglo2.includes(v))
+  return arregloFinal;
+
 }
 
 
